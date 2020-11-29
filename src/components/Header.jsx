a0f8c5b18ac2a4414aa87d1b8d54e5a98767018a
oleksandrtomesh <oleksandrtomes@gmail.com>
2020-11-29 12:29:51 +0100

    social media 0.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img className="logo" src="https://logofirmy.net/wp-content/uploads/2020/04/Huawei-Logo-2018%E2%80%93.....jpg">
+            </img>
+        </header>
+    );
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div className="profile-layout-photo">
+        <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"></img>
+      </div>
+      <div className="profile-info">
+        <div className="profile-photo">
+          <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"></img>
+        </div>
+        <div>
+          My posts
+      </div>
+      </div>
+      <div>
+        New post
+        <div>
+          post 1
+        </div>
+        <div>
+          Post 2
+        </div>
+      </div>
+      Main content
+    </div>
+  );
+}
+
+export default Profile;
