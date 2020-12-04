735dd7283cbfcd09984ad27974436d4727819f9e
oleksandrtomesh <oleksandrtomes@gmail.com>
2020-12-04 21:50:03 +0100

    props for dialogs lesson 23

--- src/components/Dialogs/Dialog/Dialog.jsx
+++ src/components/Dialogs/Dialog/Dialog.jsx
@@ -1,9 +1,10 @@
+import { NavLink, Route } from 'react-router-dom'
 import c from './Dialog.module.css'
 
 const Dialog = (props) => {
     return (
         <div className={c.dialog}>
-            {props.dialog}
+           <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink> 
         </div>
     )
 }
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,13 +6,13 @@ const Dialogs = (props) => {
     return (
         <div className={c.dialogs}>
             <div className={c.dialog}>
-                <Dialog dialog="Tania" />
-                <Dialog dialog="Pietia" /> 
-                <Dialog dialog="Sasha" /> 
-                <Dialog dialog="Vova" /> 
-                <Dialog dialog="Marina" /> 
-                <Dialog dialog="Vasia" /> 
-                <Dialog dialog="Igor" />           
+                <Dialog name="Tania" id='1'/>
+                <Dialog name="Pietia" id="2"/> 
+                <Dialog name="Sasha" id="3" /> 
+                <Dialog name="Vova" id="4" /> 
+                <Dialog name="Marina" id="5" /> 
+                <Dialog name="Vasia" id="6"/> 
+                <Dialog name="Igor" id="7"/>           
             </div>
             <div className={c.message}>
                 <Messages text="Hello" />
