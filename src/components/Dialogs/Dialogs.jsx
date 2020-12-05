aa4c7243a2cd6e8f6709a76c1892bbed0e29de1e
oleksandrtomesh <oleksandrtomes@gmail.com>
2020-12-05 21:35:00 +0100

    change project use function .map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,27 +18,19 @@ const Dialogs = (props) => {
         {id: 1, message: "Hello"},
         {id: 1, message: "How are you"}
     ]
+
+    let dialogElements = dialogData.map( d => <Dialog name={d.name} id={d.id} /> )
+
+    let messageElements = messageData.map( m => <Messages text={m.message} />)
     return (
         <div className={c.dialogs}>
             <div className={c.dialog}>
                 <h3>Dialogs</h3>
-                <Dialog 
-                name={dialogData[0].name} 
-                id={dialogData[0].id}
-                />
-                <Dialog 
-                name={dialogData[1].name} 
-                id={dialogData[1].id}
-                />
-                <Dialog 
-                name={dialogData[2].name} 
-                id={dialogData[2].id}
-                />  
+                { dialogElements }
             </div>
             <div className={c.message}>
                 <h3>Messages</h3>
-                <Messages text={messageData[0].message} />
-                <Messages text={messageData[1].message} />
+                { messageElements }
             </div>
         </div>
     )
